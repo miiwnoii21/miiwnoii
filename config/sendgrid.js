cab8e4c7b7a9bac71aad3bd42216e6e04c30656d
Boonyarit Wiriyagulpat <boonyarit@itorama.com>
2015-05-14 09:21:14 +0700

    remove sendgrid credential from public access

--- config/sendgrid.js
+++ config/sendgrid.js
@@ -1,7 +1,7 @@
 module.exports.sendgrid = {
 
   smtp: 'serversmtp.sendgrid.net',
-  user: process.env.SENDGRID_USERNAME || 'app36352024@heroku.com',
-  password: process.env.SENDGRID_PASSWORD || 'sgd22moq1978'
+  user: process.env.SENDGRID_USERNAME,
+  password: process.env.SENDGRID_PASSWORD
 
 };
